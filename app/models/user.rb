d0605df291016e65e418ce70a12c41d32d9898e3
Yih En Lim <ylim@booster.com>
2014-04-09 17:40:37 +0800

    Deleted before-action

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,6 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
-
-  before_action :authenticate_user!
-
+  
   validates :password, confirmation: true
 end
